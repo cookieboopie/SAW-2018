4ecf0b8eacce5033cb310cbae36f8f0de48656d3
cookieboopie <gabrialb96@gmail.com>
2018-07-04 21:40:03 +0200

    Form reg/log in stessa finestra

--- index.php
+++ index.php
@@ -11,16 +11,22 @@
 <section class="wrapper">
     <div class="inner">
         <form id="regForm">
-            <label for="regUsr">Username: </label>
+            <label for="regUsr">Username </label>
             <input type="text" id="regUsr" name="regUsr">
-            <label for="regMail">E-mail: </label>
+            <label for="regMail">E-mail </label>
             <input type="text" id="regMail" name="regMail">
-            <label for="regPwd">Password: </label>
+            <label for="regPwd">Password </label>
             <input type="password" id="regPwd" name="regPwd">
-            <label for="pwdC">Conferma Password: </label>
+            <label for="pwdC">Conferma Password </label>
             <input type="password" id="pwdC" name="pwdC">
             <input type="submit" id="regSubmit" value="Crea Account">
             <span id="inputError"></span>
+        </form>
+        <form id="logForm">
+            <label for="logUsr">Username </label>
+            <input type="text" id="logUsr" name="logUsr">
+            <label for="logPw">Password </label>
+            <input type="text" id="logPw" name="logPw">
         </form>
     </div>
 </section>
