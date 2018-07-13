143614028df63addedeb61dad0f58a9f4c6756df
cookieboopie <gabrialb96@gmail.com>
2018-07-13 17:12:53 +0200

    working on CSS

--- nonPiuIndex.php
+++ nonPiuIndex.php
@@ -15,33 +15,61 @@
 <section class="wrapper">
     <div class="inner">
         <div class="formHeader">
-            <ls class="reg" style="cursor: pointer">Register</ls>
-            <ls class="log" style="cursor: pointer">Login</ls>
-        </div>
-        <div class="formBody">
-            <div class="closeContainer">
-                <div class="close" style="cursor: pointer">&times</div>
+            <div class="reg" style="cursor: pointer">Register</div>
+            <div class="log" style="cursor: pointer">Login</div>
+            <div class="bodyContainer" >
+                <div class="formBody">
+                    <div class="closeContainer">
+                        <div class="close" style="cursor: pointer">&times</div>
+                    </div>
+                    <form id="regForm" autocomplete="off">
+                        <label for="regUsr">Username </label><span id="regUsr_err"></span>
+                        <input type="text" id="regUsr" name="regUsr">
+                        <label for="regEml">E-mail </label><span id="regEml_err"></span>
+                        <input type="text" id="regEml" name="regEml">
+                        <label for="regPwd">Password </label><span id="regPwd_err"></span>
+                        <input type="password" id="regPwd" name="regPwd">
+                        <label for="pwdC">Conferma Password </label><span id="pwdC_err"></span>
+                        <input type="password" id="pwdC" name="pwdC">
+                        <label> <br><br> </label>
+                        <input type="button" id="regSubmit" class="btn btn-primary" value="Crea Account">
+                        
+                    </form>
+                    <form id="logForm" autocomplete="off" > 
+                        <label for="logUsr">Username </label><span id="logUsr_err"></span>
+                        <input type="text" id="logUsr" name="logUsr">
+                        <label for="logPwd">Password </label><span id="logPwd_err"></span>
+                        <input type="password" id="logPwd" name="logPwd">
+                        <input type="button" id="logSubmit" value="Login">
+                        <span id="logErr"></span>
+                    </form>
+                </div>
             </div>
-            <form id="regForm" autocomplete="off">
-                <label for="regUsr">Username </label><span id="regUsr_err"></span>
-                <input type="text" id="regUsr" name="regUsr">
-                <label for="regEml">E-mail </label><span id="regEml_err"></span>
-                <input type="text" id="regEml" name="regEml">
-                <label for="regPwd">Password </label><span id="regPwd_err"></span>
-                <input type="password" id="regPwd" name="regPwd">
-                <label for="pwdC">Conferma Password </label><span id="pwdC_err"></span>
-                <input type="password" id="pwdC" name="pwdC">
-                <input type="submit" id="regSubmit" value="Crea Account">
-            </form>
-            <form id="logForm" autocomplete="off" > 
-                <label for="logUsr">Username </label><span id="logUsr_err"></span>
-                <input type="text" id="logUsr" name="logUsr">
-                <label for="logPwd">Password </label><span id="logPwd_err"></span>
-                <input type="password" id="logPwd" name="logPwd">
-                <input type="button" id="logSubmit" value="Login">
-                <span id="logErr"></span>
-            </form>
         </div>
+<!--        <div class="formBody">
+                <div class="closeContainer">
+                    <div class="close" style="cursor: pointer">&times</div>
+                </div>
+                <form id="regForm" autocomplete="off">
+                    <label for="regUsr">Username </label><span id="regUsr_err"></span>
+                    <input type="text" id="regUsr" name="regUsr">
+                    <label for="regEml">E-mail </label><span id="regEml_err"></span>
+                    <input type="text" id="regEml" name="regEml">
+                    <label for="regPwd">Password </label><span id="regPwd_err"></span>
+                    <input type="password" id="regPwd" name="regPwd">
+                    <label for="pwdC">Conferma Password </label><span id="pwdC_err"></span>
+                    <input type="password" id="pwdC" name="pwdC">
+                    <input type="submit" id="regSubmit" value="Crea Account">
+                </form>
+                <form id="logForm" autocomplete="off" > 
+                    <label for="logUsr">Username </label><span id="logUsr_err"></span>
+                    <input type="text" id="logUsr" name="logUsr">
+                    <label for="logPwd">Password </label><span id="logPwd_err"></span>
+                    <input type="password" id="logPwd" name="logPwd">
+                    <input type="button" id="logSubmit" value="Login">
+                    <span id="logErr"></span>
+                </form>
+            </div>-->
     </div>
 </section>
 
