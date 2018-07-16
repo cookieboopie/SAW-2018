e5482b8f883e44793bb289f3a1e73c0bd78ca84b
cookieboopie <gabrialb96@gmail.com>
2018-07-16 23:40:00 +0200

    added remember me

--- form.php
+++ form.php
@@ -46,6 +46,12 @@
                             <span></span>
                             
                             <label for="logPwd">Password </label><input type="password" id="logPwd" name="logPwd">
+                            
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Default checkbox
+                            </label>
+                            
 
                             <input type="button" id="logSubmit" class="btn btn-primary" value="Login">
                         </form>
